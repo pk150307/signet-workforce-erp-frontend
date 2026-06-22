bdc482d39ced47c8cfecd5f57d63cbffb16d4f74
Piyush kumar <48119623+pk150307@users.noreply.github.com>
2026-06-22 21:09:11 +0530

    Add files via upload

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://35.154.205.144:5000/api',
+  apiUrl: 'https://api.signetcorporateservices.com/api',
   appName: 'Signet Workforce ERP',
   appVersion: '1.0.0'
 };
